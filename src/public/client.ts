fc7dcaa016e35fbc5e7296ea316f69d294aff892
danielgstephenson <dgstephenson.econ@gmail.com>
2026-01-15 17:06:16 -0500

    token with client class

--- src/public/client.ts
+++ src/public/client.ts
@@ -5,10 +5,13 @@ export class Client {
   token = 0
 
   constructor() {
+    console.log('client')
     this.socket.on('connect', () => {
       console.log('connect')
     })
     this.socket.on('token', (token: number) => {
+      console.log('this.token', this.token)
+      console.log('token', token)
       if (this.token !== 0 && this.token !== token) {
         console.log('reload')
         location.reload()
