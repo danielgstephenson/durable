c78ddefa2191cfdaea75217e641a629b0cf457ff
danielgstephenson <dgstephenson.econ@gmail.com>
2026-01-15 17:01:03 -0500

    client class

--- src/public/client.ts
+++ src/public/client.ts
@@ -1,17 +1,20 @@
 import { io } from 'socket.io-client'
 
-const socket = io()
-let token = 0
+export class Client {
+  socket = io()
+  token = 0
 
-socket.on('connect', () => {
-  console.log('connect')
-})
-
-socket.on('token', (t: number) => {
-  if (token !== 0 && token !== t) {
-    console.log('reload')
-    location.reload()
-    return
+  constructor() {
+    this.socket.on('connect', () => {
+      console.log('connect')
+    })
+    this.socket.on('token', (token: number) => {
+      if (this.token !== 0 && this.token !== token) {
+        console.log('reload')
+        location.reload()
+        return
+      }
+      this.token = token
+    })
   }
-  token = t
-})
+}
